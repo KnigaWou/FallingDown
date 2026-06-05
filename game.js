5e061693f752fa44e65cc628589f8b049d509593
KnigaWou <bravenewfeast@gmail.com>
2026-06-05 21:53:38 +0300

    Update game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ function draw() {
         ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 
             if (trampedImage.complete && trampedImage.src) {
-            let pipeWidth = 60;
+            let pipeWidth = 100;
             let pipeX = (canvas.width - pipeWidth) / 2;
             ctx.drawImage(trampedImage, pipeX, 0, pipeWidth, canvas.height);
             }
