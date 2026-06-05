e9cfd26cea167c6ccc60324e31544abe73723fd2
KnigaWou <bravenewfeast@gmail.com>
2026-06-05 22:43:02 +0300

    Update game.js

--- game.js
+++ game.js
@@ -4,11 +4,11 @@ let ctx = canvas.getContext('2d');
 let backgroundImage = new Image();
 backgroundImage.src = 'pictures/Background.jpg';
 
-let trampedImage = new Image();
-trampedImage.src = 'pictures/tramped.png';
-
 let peopleImage = new Image();
 peopleImage.src = 'pictures/people.png';
+
+let trampedImage = new Image();
+trampedImage.src = 'pictures/tramped.png';
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -16,16 +16,17 @@ function draw() {
     if (backgroundImage.complete && backgroundImage.src) {
         ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 
-            if (trampedImage.complete && trampedImage.src) {
-            let trampedWidth = 350;
-            let trampedX = (canvas.width - trampedWidth) / 2;
-            ctx.drawImage(trampedImage, trampedX, 0, trampedWidth, canvas.height);
-
             if (peopleImage.complete && peopleImage.src) {
             let peopleWidth = 350;
             let peopleHeight = 900;
             let peopleX = (canvas.width - peopleWidth) / 2;
             ctx.drawImage(peopleImage, peopleX, 0, peopleWidth, peopleHeight);
+                
+            if (trampedImage.complete && trampedImage.src) {
+            let trampedWidth = 350;
+            let trampedX = (canvas.width - trampedWidth) / 2;
+            ctx.drawImage(trampedImage, trampedX, 0, trampedWidth, canvas.height);
+
             }
             }
     }
