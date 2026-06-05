3280be1e71015c8c2669b551dbee9c269bd5dcce
KnigaWou <bravenewfeast@gmail.com>
2026-06-05 21:34:34 +0300

    Update game.js

--- game.js
+++ game.js
@@ -4,11 +4,18 @@ let ctx = canvas.getContext('2d');
 let backgroundImage = new Image();
 backgroundImage.src = 'pictures/Background.jpg';
 
+let trampedImage = new Image();
+trampedImage.src = 'pictures/tramped.jpg';
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     if (backgroundImage.complete && backgroundImage.src) {
         ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+            if (trampedImage.complete && trampedImage.src) {
+        ctx.drawImage(trampedImage, 0, 0, canvas.width, canvas.height);
+            }
     }
 }
 
