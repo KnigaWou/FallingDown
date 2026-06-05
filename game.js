e9821b928bbbf36098167f2a7d4dda8606deb41c
KnigaWou <bravenewfeast@gmail.com>
2026-06-05 15:52:28 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,21 @@
 let canvas = document.getElementById('gameCanvas');
 let ctx = canvas.getContext('2d');
-ctx.fillStyle = 'red';
-ctx.fillRect(100, 100, 50, 50);
+let playerX = 100;
+let playerY = 100;
+
+function draw() 
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(playerX, playerY, 50, 50);
+}
+draw();
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') playerX = playerX - 10;
+    if (event.key === 'ArrowRight') playerX = playerX + 10;
+    if (event.key === 'ArrowUp') playerY = playerY - 10;
+    if (event.key === 'ArrowDown') playerY = playerY + 10;
+    
+    draw();
+});
