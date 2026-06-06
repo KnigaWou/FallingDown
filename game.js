74ceaa9d31209fed51b7e579e43260650bd6a59b
KnigaWou <bravenewfeast@gmail.com>
2026-06-06 14:20:51 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // Переменная для высоты человечка
 let peopleStateHeight = 700;
-const NEW_IMAGE_SCALE = 0.5;  // Изменить размер изображений
+const NEW_IMAGE_SCALE = 0.2;  // Изменить размер изображений
  let isBraking = false;
 
 let a = 333; // ШИРИНА РУК
